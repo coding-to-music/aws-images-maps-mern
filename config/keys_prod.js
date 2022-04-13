fc4115a7bcd97bb9687f71fd74c6ab59f869a118
coding-to-music <connors.tom@gmail.com>
2022-04-13 06:31:53 +0000

    first commit

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-  MongoUri: process.env.MONGO_URI,
+  MongoUri: process.env.MONGODB_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   S3Bucket: process.env.S3BUCKET,
   awsAccessKeyId: process.env.AWS_KEY,
-  awsSecretAccessKey: process.env.AWS_SECRET_KEY
+  awsSecretAccessKey: process.env.AWS_SECRET_KEY,
 };
